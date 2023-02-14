ab01967d4b3cc3edc21fb2b58bdadfc2bcb846e2
Diego Alejandro <diales1981@gmail.com>
2023-02-14 15:56:19 -0500

    First commit, init the bookstore app

---
+++ src/BookstoreApp.tsx
@@ -0,0 +1,8 @@
+import './App.scss'
+import { LoginPage } from './users/pages/LoginPage'
+
+export const CrmApp = () => {
+	return (
+		<LoginPage/>
+	)
+}
---
+++ src/users/pages/CreateUserPage.tsx
@@ -0,0 +1,6 @@
+
+export const CreateUserPage = () => {
+	return (
+		<div>CreateUserPage</div>
+	)
+}
